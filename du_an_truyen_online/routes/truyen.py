46611c4c5f7f6f9c7497a6c8fdc2170a5e597d75
quocdunglu123-coder <quocdunglu123@gmail.com>
2026-07-16 21:36:31 +0700

    Hoàn thành trọn bộ tính năng PUT và DELETE phân quyền admin quản lý truyện ngày 23

--- du_an_truyen_online/routes/truyen.py
+++ du_an_truyen_online/routes/truyen.py
@@ -35,3 +35,34 @@ def api_them_truyen(truyen:TruyenForm ,nguoi_dung=Depends(xac_thuc_nguoi_dung)):
     ket_noi.commit()
     ket_noi.close()
     return {"status":"Thành công","message":f"Admin '{nguoi_dung['sub']}' đã xuất bản thành công '{truyen.ten_truyen}'!"}
+
+@router_truyen.put("/sua_truyen/{truyen_id}")
+def api_sua_truyen(truyen_id:int,truyen:TruyenForm,nguoi_dung=Depends(xac_thuc_nguoi_dung)):
+    if nguoi_dung.get("role")!="admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Thất bại ! Bạn không có đặc quyền Admin để chỉnh sửa nội dung Truyện!"
+        )
+    ket_noi=lay_ket_noi()
+    con_tro=ket_noi.cursor()
+    con_tro.execute(
+        "UPDATE truyen SET ten_truyen=?,tac_gia=?,noi_dung=? WHERE id=?",
+        (truyen.ten_truyen,truyen.noi_dung,truyen.tac_gia,truyen_id)
+    )
+    ket_noi.commit()
+    ket_noi.close()
+    return{"status":"Thành công","message":f"Admin '{nguoi_dung['sub']}' đã cập nhật truyện ID số {truyen_id} vĩnh viễn!"}
+
+@router_truyen.delete("/xoa-truyen/{id_truyen}")
+def api_xoa_truyen(truyen_id:int,nguoi_dung=Depends(xac_thuc_nguoi_dung)):
+    if nguoi_dung.get("role") !="admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Thất bại! Bạn không có đặc quyền Admin để xóa truyện!"
+        )
+    ket_noi=lay_ket_noi()
+    con_tro=ket_noi.cursor()
+    con_tro.execute("DELETE FROM truyen WHERE id=?",(truyen_id,))
+    ket_noi.commit()
+    ket_noi.close()
+    return{"status":"Thành công","message":f"Admin '{nguoi_dung['sub']}' đã xóa hoàn toàn truyện ID số {truyen_id}!"}
